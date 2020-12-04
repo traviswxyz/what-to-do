7358f1b18b1446f08828e924c1404f9e17618881
Travis <travis@Traviss-MacBook-Pro.local>
2020-12-03 19:54:39 -0500

    collapse/reveal button broken

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ const count = document.getElementById("count-badge");
 const apiKey = document.getElementById("apiKey");
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(window.location.search);
-
+const historyTable = document.getElementById("historyTable");
+const history = document.getElementById("history");
 
 btn.addEventListener("click", async function() {
   if(urlParams.get("pexels") !== null){
@@ -35,15 +36,33 @@ btn.addEventListener("click", async function() {
   document.getElementById("activityLabel").innerText = activity;
   choiceBox1.appendChild(await fetchImg(activity));
 
-  let city = random(cities);
-  document.getElementById("cityLabel").innerText = city;
-  choiceBox2.appendChild(await fetchImg(city));
+  let place = random(cities);
+  document.getElementById("cityLabel").innerText = place;
+  choiceBox2.appendChild(await fetchImg(place));
 
   let food = random(foods);
   document.getElementById("foodLabel").innerText = food;
   choiceBox3.appendChild(await fetchImg(food));
 
   count.innerText++;
+  let row = document.createElement("tr");
+  let suggestionCount = document.createElement("td");
+  suggestionCount.innerText=count.innerText;
+  row.appendChild(suggestionCount);
+
+  let foodHistory = document.createElement("td");
+  foodHistory.innerText=food;
+  row.appendChild(foodHistory);
+
+  let placeHistory = document.createElement("td");
+  placeHistory.innerText=place;
+  row.appendChild(placeHistory);
+  
+  let activityHistory = document.createElement("td");
+  activityHistory.innerText=activity;
+  row.appendChild(activityHistory);
+
+  history.appendChild(row);  
 });
 
 function random(choices) {
@@ -51,8 +70,8 @@ function random(choices) {
   return choices[index];
 }
 
-async function fetchImg(choice){
-  return fetch("https://api.pexels.com/v1/search?query=" + choice, {
+async function fetchImg(randomChoice){
+  return fetch("https://api.pexels.com/v1/search?query=" + randomChoice, {
   method: 'GET',
   headers: {
     'Authorization': apiKey.value,
@@ -64,20 +83,20 @@ async function fetchImg(choice){
   let div = document.createElement("div");
   let img = document.createElement("img");
 
-  let btn = document.createElement("a");
-  btn.innerText="More about " + choice;
-  btn.href="https://www.google.com/search?q=" + choice;
-  btn.className="more-btn mt-3 btn btn-sm btn-block btn-outline-primary";
-  btn.target="_blank";
+  let moreInfoBtn = document.createElement("a");
+  moreInfoBtn.innerText="More about " + randomChoice;
+  moreInfoBtn.href="https://www.google.com/search?q=" + randomChoice;
+  moreInfoBtn.className="more-btn mt-3 btn btn-sm btn-block btn-outline-primary";
+  moreInfoBtn.target="_blank";
 
   let index = Math.floor(Math.random() * data.photos.length);
-  console.log(choice + " " + index);
+  console.log(randomChoice + " " + index);
   img.src=data.photos[index].src.tiny;
-  img.alt=choice;
+  img.alt=randomChoice;
   img.className="img-fluid";
 
   div.appendChild(img);
-  div.appendChild(btn);
+  div.appendChild(moreInfoBtn);
 
   return div;
 })
